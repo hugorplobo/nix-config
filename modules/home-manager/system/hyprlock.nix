5035b5861bc6646b9a4dc60a859b5622baee4d3c
Hugo Lobo <hugorplobo@gmail.com>
2024-09-08 16:19:58 -0300

    update hyprlock

--- modules/home-manager/system/hyprlock.nix
+++ modules/home-manager/system/hyprlock.nix
@@ -7,41 +7,35 @@
             background = [
                 {
                     path = "${../../../static/wallpaper.png}";
-                    blur_passes = 4;
-                    blur_size = 2;
+                }
+            ];
+
+            image = [
+                {
+                    path = "${../../../static/wallpaper_nobg.png}";
+                    size = 768;
+                    rounding = 0;
+                    border_size = 0;
+                    valign = "center";
+                    halign = "center";
+                    position = "0, 0";
+                    zindex = 10;
+                    shadow_passes = 2;
+                    shadow_size = 4;
+                    shadow_boost = 0.5;
                 }
             ];
 
             label = [
                 {
-                    text = ''<b>Quixadá ☀️ | 37° C</b>'';
+                    text = ''cmd[update:1000] echo -e "$(date +"%H:%M")"'';
                     text_align = "center";
                     color = "rgba(255, 255, 255, 1.0)";
-                    font_size = 13;
-                    font_family = "FiraCode Nerd Font Mono";
-                    valign = "top";
-                    halign = "center";
-                    position = "0, -20";
-                }
-                {
-                    text = ''cmd[update:1000] echo -e "$(date +"%H")"'';
-                    text_align = "center";
-                    color = "rgba(255, 255, 255, 1.0)";
-                    font_size = 150;
+                    font_size = 160;
                     font_family = "Alpha Slab One";
                     valign = "center";
-                    halign = "center";
-                    position = "0, 85";
-                }
-                {
-                    text = ''cmd[update:1000] echo -e "$(date +"%M")"'';
-                    text_align = "center";
-                    color = "rgba(255, 255, 255, 1.0)";
-                    font_size = 150;
-                    font_family = "Alpha Slab One";
-                    valign = "center";
-                    halign = "center";
-                    position = "0, -85";
+                    halign = "left";
+                    position = "25, 0";
                 }
                 {
                     text = ''cmd[update:1000] echo -e "<b>$(date +"%d %b, %A")</b>"'';
@@ -50,8 +44,8 @@
                     font_size = 13;
                     font_family = "FiraCode Nerd Font Mono";
                     valign = "center";
-                    halign = "center";
-                    position = "0, -180";
+                    halign = "left";
+                    position = "35, -100";
                 }
             ];
         };
